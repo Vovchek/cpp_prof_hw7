2c233e4cd33dd171efa5b8a35e071928400f9df1
Vovchek <39309092+Vovchek@users.noreply.github.com>
2023-04-05 20:21:07 +0300

    cure path in doxyfile

--- bulk.cpp
+++ bulk.cpp
@@ -1,6 +1,13 @@
-/*! \mainpage Homework #7 Syllabus.
+/**
+ * @file bulk.cpp
+ * @brief command batches processor. Detects bulks and writes them both to cout and files.
+ * @author Vladimir Chekal
+ * @date April 2023
 
-Разработать программу для пакетной обработки команд.\n
+ * @mainpage Homework #7 Syllabus.
+ * @anchor business_logic
+ *
+\nРазработать программу для пакетной обработки команд.\n
 Команды считываются построчно из стандартного ввода и обрабатываются блоками по N команд.\n
 Одна команда - одна строка, конкретное значение роли не играет. Если данные закончились - блок\n
 завершается принудительно. Параметр N передается как единственный параметр командной\n
@@ -77,29 +84,55 @@ bulk1517223860.log , где 1517223860 - это время получения п
 #include <chrono>
 #include <sstream>
 
-/// @brief
+/**
+ * @brief Class to store and manipulate detected commands sequence.
+ * @details
+ *  Manages container where bulk of comands are stored.\n
+ *  Comands may be inserted, printed on console and serialized to a file.
+ */
 class Bulk
 {
-    std::vector<std::string> data;
-    std::string fn;
+private:
+    std::vector<std::string> data; /**< Container storing commands */
+    std::string fn; /**< File name to save this bulk. */
 
 public:
+    /** @brief Number of commands stored.
+     * @returns container size, storing entered commands.
+     */
     size_t size() const
     {
         return data.size();
     }
+    /**
+     * @brief Inters a command at the end of a bulk.
+     * @param cmd [in] The command string to be inserted.
+     * @details Inters a command, represented by a refference to string, at the end of a bulk.
+     *          When inserting the first command (supposedly) unique file name is generated and
+     *          stored using steady clock count.
+     */
     void insert(const std::string &cmd)
     {
         if(!size())
             fn = time_to_filename(std::chrono::steady_clock::now());
         data.push_back(cmd);
     }
+    /**
+     * @brief Write bulk first to console, then to file.
+     * @details After displaying bulk on the console data is saved to a file with
+     *          a name generated at the moment whn first command was inserted.
+    */
     void output() const
     {
         write_to(std::cout);
         std::ofstream f{fn};
         write_to(f);
     }
+    /**
+     * @brief Output all commands from a buffer to the specified stream.
+     * @param [in] os Stream to write commands.
+     * @details Commands are writen in one line starting with `bulk: ', comma plus whitespace (', ') separated.
+    */
     void write_to(std::ostream &os) const {
         if (size())
         {
@@ -113,15 +146,28 @@ public:
             os << '\n';
         }
     }
+    /** @brief Erases contents of a bulk object.
+     * @details Deletes all elements (commands).
+     */
     void clear()
     {
         data.clear();
     }
+    /** @brief Outputs and then erases contents of a bulk object.
+     * @details Stored commands first are output to console and then to file.\n
+     *          Then the bulk contents are erased.
+     */
     void flush()
     {
         output();
         clear();
     }
+    /**
+     * @brief Create file name string based on time point.
+     * @param [in] t Reference to time_point structure.
+     * @details Converts time point to a readable string of a microseconds since epoch start,
+     *           then combines file name starting with "bulk" and ending on ".log" extension.
+    */
     std::string time_to_filename(const std::chrono::time_point<std::chrono::steady_clock> &t) const {
         std::string fn {"bulk" +
             std::to_string(
@@ -130,14 +176,20 @@ public:
         return fn;
     }
 };
-
+/**
+ * @brief main function that implements business logic.
+ * @param argc [in] Number of a program parameters strings.
+ * @param argv [in] Array of pointers to c-style char arrays, storing program parameters
+ * @returns Exit status - 0 (EXIT_SUCCESS) on success, non-zero (EXIT_FAILURE) on failure.
+ * @details Almost all business logic implementation is withing main() function. See \ref business_logic "mainpage" for detailed description.
+*/
 int main(int argc, char const *argv[])
 {
 
     size_t N{3};
     if (argc < 2 || (N = std::stoi(argv[1])) <= 0)
     {
-        std::cerr << "Should specify positive bulk size as a program parameter (e.g. bulk.exe 3)\n";
+        std::cerr << "Should specify positive bulk size as a program parameter (e.g. `>bulk.exe 3`)\n";
         return EXIT_FAILURE;
     }
 
